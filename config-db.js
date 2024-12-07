5012b4cd2606c8a8a28f9c697b7d61996dedefaf
Sunny Das <114607514+sunnydas16@users.noreply.github.com>
2024-12-07 12:55:40 +0530

    Create config-db.js
    final learning

---
+++ config-db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const connection = mongoose.connect('mongodb://0.0.0.0/men').then( () => {
+    console.log('connected to database')
+})
+
+module.exports = connection
